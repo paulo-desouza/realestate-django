8101cb3cbdbe5513a2f1f935eae1d1325978ee24
Paulo de Souza <paulo@desouza-tech.com>
2024-02-17 14:47:05 -0500

    substitute POSTGRESQL for sqlite

--- RealtorSite/RealtorSite/settings.py
+++ RealtorSite/RealtorSite/settings.py
@@ -54,19 +54,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'RealtorSite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'realtordb',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
