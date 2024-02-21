5a37f33fe0f8fddbc2e34e002ac2ac3de1f08fed
Paulo de Souza <paulo@desouza-tech.com>
2024-02-21 12:48:20 -0500

    Settings: static dirs

--- RealtorSite/RealtorSite/settings.py
+++ RealtorSite/RealtorSite/settings.py
@@ -95,11 +95,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'realtorsite/static')
-]
+STATIC_ROOT = '/static/'
+STATIC_URL = '/static/'
+
 
 
 # Media Folder Settings
